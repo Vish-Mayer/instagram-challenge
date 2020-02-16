7b87f8f4016faa1d58a1e1d7d08f4a3895c8d14c
Vish Mayer <vish.mayer@gmail.com>
2020-02-16 14:32:35 +0000

    sign in/up routes to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :accounts
-root to: "public#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/dashboard" => "accounts#index"
+
+  root to: "public#homepage"
 end
